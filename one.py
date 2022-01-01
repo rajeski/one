0861b846abf6e2bceea8dc85738a4b7406952734
John S. Rajeski, Ed.D <48130054+rajeski@users.noreply.github.com>
2022-01-01 18:57:36 +0900

    File Revisions
    Minor file edits.

--- one.py
+++ one.py
@@ -1,14 +1,14 @@
-#1. Program greeting.
+#1. Program greetings.
 print("Good day! Welcome to the Band Generator.")
 
-#2. Where did you grow-up.
+#2. Hometown.
 city = input("Please tell us your city name. \n")
 
-#3. What is the name of a pet.
+#3. Name of a pet.
 pet = input("Please tell us your pet's name. \n")
 
-#4. Combine these two together.
+#4. Combine the two names together.
 band_name = f"{city} {pet}"
 
-#5. Results.
+#5. Program results.
 print(f"Your band name should be {band_name}.")
